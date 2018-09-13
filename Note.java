c3cbba73b2472778957d689fbb1c7bffca66a005
Lance <ngtlancezhu@gmail.com>
2018-09-13 09:33:59 -0700

    new

--- Note.java
+++ Note.java
@@ -1,26 +1,27 @@
 package com.example.synthesizer;
 
 public class Note {
-    private int noteID;
-    private int delay;
 
+    private int noteId; //maybe make this a list of noteId that would all be played at once?
+    private int delay;
     public static final int WHOLE_NOTE = 1000;
 
-    public Note(int noteID) {
-        this.noteID = noteID;
+    public Note(int noteId) {
+        this.noteId = noteId;
+        delay = WHOLE_NOTE;
     }
 
-    public Note(int noteID, int delay) {
-        this.noteID = noteID;
-        this.delay = WHOLE_NOTE;
+    public Note(int noteId, int delay) {
+        this.noteId = noteId;
+        this.delay = delay;
     }
 
-    public int getNoteID() {
-        return noteID;
+    public int getNoteId() {
+        return noteId;
     }
 
-    public void setNoteID(int noteID) {
-        this.noteID = noteID;
+    public void setNoteId(int noteId) {
+        this.noteId = noteId;
     }
 
     public int getDelay() {
@@ -34,7 +35,7 @@ public class Note {
     @Override
     public String toString() {
         return "Note{" +
-                "noteID=" + noteID +
+                "noteId=" + noteId +
                 ", delay=" + delay +
                 '}';
     }
--- Song.java
+++ Song.java
@@ -7,11 +7,30 @@ public class Song {
 
     private List<Note> notes;
 
-public Song(){
-    notes = new ArrayList<>();
-}
+    public Song() {
+        notes = new ArrayList<>();
+    }
 
     public Song(List<Note> notes) {
         this.notes = notes;
+    }
+
+    public void add(Note note) {
+        notes.add(note);
+    }
+
+    public List<Note> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "notes=" + notes +
+                '}';
     }
 }
